b8ab33e0e8ebd22581a9eb2d91964e1029261a61
Aaron Marvin Aco <40295780+CXL58APj@users.noreply.github.com>
2022-05-01 16:48:56 +0800

    updated breadcrumb

--- meally_webadmin/pages/systemusers.php
+++ meally_webadmin/pages/systemusers.php
@@ -116,8 +116,8 @@
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
             <li class="breadcrumb-item text-sm text-dark active" aria-current="page">System Management</li>
+            <li class="font-weight-bolder mb-0 breadcrumb-item text-sm text-dark active" aria-current="page">Users</li>
           </ol>
-          <h6 class="font-weight-bolder mb-0">Users</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
@@ -186,8 +186,8 @@
         <div class="col-12">
           <div class="card my-4">
             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-              <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                <h6 class="text-white text-capitalize ps-3">Users</h6>
+              <div class="bg-gradient-primary shadow-primary border-radius-lg pt-3 pb-2">
+                <h6 class="text-white text-capitalize ps-3"> List of Users</h6>
               </div>
             </div>
             <div class="card-body px-5">
